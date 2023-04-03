55307ce720d539a26863e1043354f81e4bbbf7b5
bia1708 <bianca.popu@gmail.com>
2023-04-04 00:09:30 +0300

    Update App.tsx

--- react-frontend/src/App.tsx
+++ react-frontend/src/App.tsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react'
-import MoviesShowAll from './components/MoviesShowAll'
+import { MoviesShowAll } from './components/MoviesShowAll'
 
 function App() {
   const [count, setCount] = useState(0)
