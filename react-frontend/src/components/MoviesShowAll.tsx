095e6d3e116e718cd493b1aca3e47b28abc71423
bia1708 <bianca.popu@gmail.com>
2023-04-04 00:46:01 +0300

    Update MoviesShowAll.tsx

--- react-frontend/src/components/MoviesShowAll.tsx
+++ react-frontend/src/components/MoviesShowAll.tsx
@@ -1,36 +1,42 @@
-import { useState } from "react"
+import { useEffect, useState } from "react";
 import { Movie } from "../models/Movie";
 
 export const MoviesShowAll = () => {
     const [movies, setMovies] = useState([]);
 
-    fetch("http://http://ec2-13-50-231-92.eu-north-1.compute.amazonaws.com/movies/")
-        .then((res) => res.json())
-        .then((data) => setMovies(data));
+    useEffect(() => {
+        fetch("http://ec2-13-50-231-92.eu-north-1.compute.amazonaws.com/movies/")
+            .then((res) => res.json())
+            .then((data) => setMovies(data));
+    }, []);
+
+    if (movies.length == 0) {
+        return <div>No Movies</div>;
+    }
 
     return (
-      <div className="App">
-        <h1>Movies List</h1>
-        <table>
-            <tr>
-            <th>Title</th>
-            <th>Release Date</th>
-            <th>Director</th>
-            <th>Imdb Score</th>
-            <th>Votes</th>
-            </tr>
-            {movies.map((movie : Movie, index) => (
+        <div className="App">
+            <h1>Movies List</h1>
+            <table>
                 <tr>
-                    <td>(index)</td>
-                    <td>(movie.movie_text)</td>
-                    <td>(movie.release_date)</td>
-                    <td>(movie.director.director_name)</td>
-                    <td>(movie.imdb_score)</td>
-                    <td>(movie.votes)</td>
+                    <th>#</th>
+                    <th>Title</th>
+                    <th>Release Date</th>
+                    <th>Director</th>
+                    <th>Imdb Score</th>
+                    <th>Votes</th>
                 </tr>
-            ))}
-        </table>
-      </div>
-    )
-  }
-
+                {movies.map((movie: Movie, index) => (
+                    <tr key={index}>
+                        <td>{index}</td>
+                        <td>{movie.movie_text}</td>
+                        <td>{movie.release_date}</td>
+                        <td>{movie.director.director_name}</td>
+                        <td>{movie.imdb_score}</td>
+                        <td>{movie.votes}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    );
+};
